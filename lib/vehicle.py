c47ce109efdb0988d595b434a91f7e54f8506d22
1Wambugu <martingakuya@gmail.com>
2023-10-20 10:55:23 +0300

    Completed lab

--- lib/vehicle.py
+++ lib/vehicle.py
@@ -1,2 +1,13 @@
+# lib/vehicle.py
+
 class Vehicle:
-    pass
+    def __init__(self):
+        self.wheel_size = 0
+        self.wheel_number = 0
+
+    def go(self):
+        return "vrrrrrrrooom!"
+
+    def fill_up_tank(self):
+        return "filling up!"
+
